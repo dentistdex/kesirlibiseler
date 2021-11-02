ff7b85880f75afc3eb91576038b962e6b67291b8
Semih Çalışkan <dtsemihcaliskan@gmail.com>
2021-11-03 02:52:12 +0300

    kesirleri öğreniyorum

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int myPoundValue = 1 ;
+        double myKilogramValue = 0.45359237d ;
+
+        System.out.println("5 pound = " + (5 * myPoundValue * myKilogramValue) + " kilogram" );
+
+    }
+}
